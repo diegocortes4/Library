5c87fa143e00f8656b27a0bcaebe5ec13d9a0504
Diego Cortes <diegocortes4@gmail.com>
2024-08-06 03:43:57 +0000

    Book

---
+++ src/main/java/Book.java
@@ -0,0 +1,28 @@
+
+public class Book {
+    private String title;
+    private String author;
+
+    // Constructor
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+               "title='" + title + '\'' +
+               ", author='" + author + '\'' +
+               '}';
+    }
+}
