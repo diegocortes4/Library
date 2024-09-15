47456afb256f6eace2d994bf4339c96a6d25f790
Diego Cortes <diegocortes4@gmail.com>
2024-09-15 20:16:07 +0000

    Book

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,12 +1,13 @@
-
 public class Book {
     private String title;
     private String author;
+    private String isbn;
 
     // Constructor
-    public Book(String title, String author) {
+    public Book(String title, String author, String isbn) {
         this.title = title;
         this.author = author;
+        this.isbn = isbn;
     }
 
     // Getters
@@ -18,11 +19,33 @@ public class Book {
         return author;
     }
 
+    public String getIsbn() {
+        return isbn;
+    }
+
+    // Override toString() to display book details
     @Override
     public String toString() {
         return "Book{" +
                "title='" + title + '\'' +
                ", author='" + author + '\'' +
+               ", isbn='" + isbn + '\'' +
                '}';
+    }
+
+    // Override equals() and hashCode() for comparison when removing
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Book book = (Book) obj;
+
+        return isbn != null ? isbn.equals(book.isbn) : book.isbn == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return isbn != null ? isbn.hashCode() : 0;
     }
 }
